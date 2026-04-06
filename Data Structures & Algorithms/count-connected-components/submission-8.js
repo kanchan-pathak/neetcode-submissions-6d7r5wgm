8acf86807faf6c9ce0bd422e6aa41b4f09f00d4b
kanchan-pathak <66899600+kanchan-pathak@users.noreply.github.com>
2026-04-06 12:47:36 +0530

    Add: count-connected-components - submission-8

---
+++ Data Structures & Algorithms/count-connected-components/submission-8.js
@@ -0,0 +1,52 @@
+// dsu is used to find connected components
+// dsu checks if my parent and ur parent are same we connected
+class DSU{
+    constructor(n){
+        this.parent=Array.from({length:n+1},(_,i)=>i);
+        this.rank=Array.from({length:n+1},()=>0);
+    }
+    find(x){
+        //cutting short the line/connection between node and its highest ancestor
+        if(this.parent[x]!==x){
+            this.parent[x]=this.find(this.parent[x]);
+        }
+        return this.parent[x];
+    }
+    union(x,y){
+        let rootX=this.find(x);
+        let rootY=this.find(y);
+        // meaning they are already connected
+        if(rootX===rootY) return false;
+        if(this.rank[rootX]>this.rank[rootY]){
+            this.parent[rootY]=rootX;
+        }
+        else if(this.rank[rootX]<this.rank[rootY]){
+            this.parent[rootX]=rootY;
+        }
+        else{
+            this.parent[rootY]=rootX;
+            this.rank[rootX]+=1;
+        }
+        // they were not connected before but now they are
+        return true;
+    }
+}
+class Solution {
+    /**
+     * @param {number} n
+     * @param {number[][]} edges
+     * @returns {number}
+     */
+    countComponents(n, edges) {
+        let dsu=new DSU(n);
+        //initially all nodes are individual components
+        let components=n;
+        for(let [u,v] of edges){
+            // if they get connected, we reduce from overall components count
+            if(dsu.union(u,v)){
+                components-=1;
+            }
+        }
+        return components;
+    }
+}
