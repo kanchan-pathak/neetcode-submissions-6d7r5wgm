81802d0678df60dbc91acfa8a804d6d6df05d32d
kanchan-pathak <66899600+kanchan-pathak@users.noreply.github.com>
2026-04-13 10:43:14 +0530

    Add: word-break - submission-2

---
+++ Data Structures & Algorithms/word-break/submission-2.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string[]} wordDict
+     * @return {boolean}
+     */
+    wordBreak(s, wordDict) {
+        let n=s.length;
+        //dp definition: dp[i] means can s[0 to i-1] be split into dictionary words.
+        let dp=Array(n+1).fill(false);
+        //empty string exists in dictionary
+        dp[0]=true;
+        // i is the end limit
+        for(let i=1;i<=n;i++){
+            // j is start, so we check for substring 0...i, 1...i, so on
+            for(let j=0;j<n;j++){
+                // dp[4] is true and substring 4-8, code is also in dictionary
+                if(dp[j] && wordDict.includes(s.substring(j,i))){
+                    dp[i]=true;
+                    break;
+                }
+            }
+        }
+        return dp[n];    
+    }
+}
