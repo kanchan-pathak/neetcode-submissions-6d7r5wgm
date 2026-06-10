14a44938ec2c30a30a7e74f5d75b37174a3250e6
kanchan-pathak <66899600+kanchan-pathak@users.noreply.github.com>
2026-06-10 23:40:49 +0530

    Add: lowest-common-ancestor-in-binary-search-tree - submission-2

---
+++ Data Structures & Algorithms/lowest-common-ancestor-in-binary-search-tree/submission-2.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @param {TreeNode} p
+     * @param {TreeNode} q
+     * @return {TreeNode}
+     */
+    lowestCommonAncestor(root, p, q) {
+            //if we reach null or if we find p or q
+            //if root is p or q
+            if(!root || root.val===p.val || root.val===q.val){
+                return root;
+            }
+            let left=this.lowestCommonAncestor(root.left,p,q);
+            let right=this.lowestCommonAncestor(root.right,p,q);
+            //if we have found both p and q in splitted branches,
+            //then current node is the LCA
+            if(left && right) return root;
+            //else return one of them (one is ancestor of other)
+            return left? left:right;
+    }
+}
