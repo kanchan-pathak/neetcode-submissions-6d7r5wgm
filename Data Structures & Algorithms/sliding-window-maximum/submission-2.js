84dc199a073cfd2d68797fa2281b863b712895f3
kanchan-pathak <66899600+kanchan-pathak@users.noreply.github.com>
2026-07-22 16:12:10 +0530

    Add: sliding-window-maximum - submission-2

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-2.js
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    maxSlidingWindow(nums, k) {
+        const heap = new MaxPriorityQueue((x) => x[0]);        
+        let n=nums.length;
+        let res=[];
+        //initial window
+        for(let i=0;i<k;i++){
+            heap.enqueue([nums[i],i]);
+        }
+        res.push(heap.front()[0]);
+        let l=1,r=k;
+        while(r<n){
+            heap.enqueue([nums[r],r]);
+            while(heap.front()[1]<l){
+                heap.dequeue();
+            }
+            res.push(heap.front()[0]);
+            // our r and l both move together keeping window size intact
+            l++;
+            r++;
+        }
+        return res;
+    }
+} 
